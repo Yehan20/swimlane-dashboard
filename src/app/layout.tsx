522f298494eb9eeed1fac3b547380a6b1de17918
Yehan20 <yehansk15@gmail.com>
2025-07-19 03:36:18 +0530

    add subsets for font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import HeaderSideBarWrapper from "@/components/HeaderSideBarWrapper";
 const poppins = Poppins({
   variable: "--font-poppins",
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets:['latin']
 
 });
 
